4ac078566ac6e01eecfd00e810084d4c0b8c2761
akshat-tahalani <tahalani.akshat@gmail.com>
2026-04-24 19:41:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.py
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -5,6 +5,13 @@ class Solution:
         seen =dict()
         total = 0
 
+        #my intuition was right to use hjasmpas to track the frequency of tasks
+        #in thi s question our entire formula depends upon the the most frequent element 
+        #so the intervals and the n is going to cvhange the output because of the most freq elemnets 
+        #then we try to hit and trial the formula of the intervals
+        #then at the ned we have to make sure the intervals are more than the length of the number 
+        #of tasks otherwise its not possible
+
         for i in tasks:
             if i not in seen:
                 seen[i] = 1 
